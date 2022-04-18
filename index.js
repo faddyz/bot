55d9ae911befc3bbd54b9ff01755347de246af89
faddyz <103917645+faddyz@users.noreply.github.com>
2022-04-18 14:00:13 +0300

    vfngdfghj

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config({path: __dirname + '/.env'})
 const client = new tmi.Client({
 	options: { debug: true },
 	identity: {
-		username: 'MaesOneBOT',
-		password: 'oauth:vxh719e03arfqgl7aqil89dg2w9lhj'
+			username: 'MaesONE',
+		password: 'oauth:gg3p17y3ifnnd0lrj06u0d5fcmf8yi'
 	},
 	channels: [ 'Rogu' ]
 });
@@ -18,6 +18,11 @@ if(message.toLowerCase() === '!test') {
 }
 if (message.toLowerCase() === '!rank') {
  rankFetch(channel)
+
+}
+if (message.toLowerCase() === '!valorank') {
+
+ valorankFetch(channel)
 }
 });
 const rankFetch = async (channel) => {
@@ -29,16 +34,32 @@ const id = JSON.parse(await result.text()).id
 const rankUrl = await `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/bWmanG6qX1aYhm0O37J6ajXRKgsoutgnnkexqQah4XwGxH0?api_key=RGAPI-d7b061ab-14cd-42e8-821b-fa5c707fbeb2`
 result = await fetch(rankUrl)	
 data = JSON.parse(await result.text())
-rankSpeak(channel,data)
-}
-const rankSpeak = (channel,data) => {
-console.log(channel)
-console.log(data)
-
-
-
 const {summonerName, tier, rank, leaguePoints } = data[2];
-
-
-client.say(channel, `${summonerName} şu anda ${tier} ${rank} ve ${leaguePoints} LP'si var.`);
+speak(channel,`${summonerName} şu anda ${tier} ${rank} ve ${leaguePoints} LP'si var.`)
 }
+const speak = (channel,text) => {
+console.log(channel)
+console.log(text)
+
+
+
+
+
+
+client.say(channel, text);
+}
+
+const valorankFetch = async (channel) => {
+
+ 
+const accountUrl = `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/fba1aaea-1e89-51c9-8ff5-04b43fd781df`
+let result = await fetch(accountUrl)
+const id = JSON.parse(await result.text()).id
+const rankUrl = await `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/fba1aaea-1e89-51c9-8ff5-04b43fd781df`
+result = await fetch(rankUrl)	
+data = JSON.parse(await result.text())
+const {currenttierpatched,ranking_in_tier,name} = data.data;
+client.say(channel,` ${name} şu an ${currenttierpatched} ${ranking_in_tier} puanda.`)
+}
+
+
