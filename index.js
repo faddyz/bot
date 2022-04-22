ccf0f4d6c146ce2c1cc671df8b391beaffc98334
faddyz <103917645+faddyz@users.noreply.github.com>
2022-04-23 02:30:29 +0300

    cd

--- index.js
+++ index.js
@@ -8,22 +8,20 @@ const client = new tmi.Client({
 			username: 'MaesOneBOT',
 		password: 'oauth:vxh719e03arfqgl7aqil89dg2w9lhj'
 	},
-	channels: [ 'Rogu' ]
+	channels: [ 'maesone' ]
 });
+
 client.connect();
+{
+var active = true;
+
 client.on('message', (channel, tags, message, self) => {
-if(self) return;
-if(message.toLowerCase() === '!test') {
- client.say(channel, `Test!`);
-}
-if (message.toLowerCase() === '!rank') {
+	if(message.toLowerCase() === '!rank') {
+	
+
 	rankFetch(channel,tags)
- 
- valorankFetch(channel,tags)
 }
 });
-
-
 const rankFetch = async (channel,tags) => {
 
  
@@ -33,63 +31,89 @@ const rankFetch = async (channel,tags) => {
 	const rankUrl = await `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/bWmanG6qX1aYhm0O37J6ajXRKgsoutgnnkexqQah4XwGxH0?api_key=RGAPI-87079135-fd2e-4430-973b-9af76f409e64`
 	result = await fetch(rankUrl)	
 	data = JSON.parse(await result.text())
+	if (active) {
 	const arr = data;
 	const obj = arr.find((o) => o.queueType === "RANKED_SOLO_5x5");
-		const {summonerName, tier, rank, leaguePoints } = obj;
-		speak(channel,`LoL:" ${summonerName} şu anda ${tier} ${rank} ve ${leaguePoints} LP'si var. @${tags.username} " `)
-	}
-	const speak = (channel,text) => {
-	console.log(channel)
-	console.log(text)
-	
-	
-	
-	
-	
-	
-	client.say(channel, text);
-	}
+	const {summonerName, tier, rank, leaguePoints } = obj;		
+	client.say(channel,`LoL:" ${summonerName} şu anda ${tier} ${rank} ve ${leaguePoints} LP'si var. @${tags.username} " `)
+active = false;
+setTimeout(() => {
+	active = true;
+}, 15000);
+} else {
+console.log(` cooldown` );
+}
+}
+}
 
-
-const valorankFetch = async (channel,tags) => {
+{	
+	var active2 = true;
+	
+	client.on('message', (channel, tags, message, self) => {
+		if (message.toLowerCase() === '!rank') {
+		
+	
+		valorankFetch(channel,tags)
+	}
+	});
+	const valorankFetch = async (channel,tags) => {
 
  
-	const accountUrl = `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/fba1aaea-1e89-51c9-8ff5-04b43fd781df`
-let result = await fetch(accountUrl)
-const id = JSON.parse(await result.text()).id
-const rankUrl = await `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/fba1aaea-1e89-51c9-8ff5-04b43fd781df`
-result = await fetch(rankUrl)	
-valdata = JSON.parse(await result.text())
+		const accountUrl = `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/fba1aaea-1e89-51c9-8ff5-04b43fd781df`
+	let result = await fetch(accountUrl)
+	const id = JSON.parse(await result.text()).id
+	const rankUrl = await `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/fba1aaea-1e89-51c9-8ff5-04b43fd781df`
+	result = await fetch(rankUrl)	
+	valdata = JSON.parse(await result.text())
+	if (active2) {
+		function intervalFunc(data) {
+	
+			const {currenttierpatched,ranking_in_tier,name} = valdata.data;
+			client.say(channel,`Valo: " ${name} şu an ${currenttierpatched} ${ranking_in_tier} puanda. @${tags.username} " `)
+		}
+		setTimeout(intervalFunc, 4000);
+			active2 = false;
+			setTimeout(() => {
+				active2 = true;
+			}, 15000);
+		} else {
+			console.log(` cooldown` );
+		}
+		
+	
+	}}
 
-	function intervalFunc(data) {
 
-		const {currenttierpatched,ranking_in_tier,name} = valdata.data;
-		client.say(channel,`Valo: " ${name} şu an ${currenttierpatched} ${ranking_in_tier} puanda. @${tags.username} " `)
-	}
+{	
+var active1 = true;
+
+client.on('message', (channel, tags, message, self) => {
+	if(message.toLowerCase() === '!valosonmaç') {
 	
 
-
-	  setTimeout(intervalFunc, 4000);
-	}
-
-
-
-
-	client.on('message', (channel, tags, message, self) => {
-	if(message.toLowerCase() === '!valosonmaç')
 	lastFetch(channel,tags)
+}
 });
 	const lastFetch = async (channel,tags) => {
 	 
 	  const macUrl = await `https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/fba1aaea-1e89-51c9-8ff5-04b43fd781df`
 result = await fetch(macUrl)	
 valdata = JSON.parse(await result.text())
+if (active1) {
 const {mmr_change_to_last_game,ranking_in_tier} = valdata.data;
 	{
       if(mmr_change_to_last_game<0)
 		client.say(channel,`Valorant son maç: "Lose" ve giden puan "${mmr_change_to_last_game}"; Güncel puan: "${ranking_in_tier}" .`)
 		else 
 		client.say(channel,`Valorant son maç "Win" ve gelen puan "${mmr_change_to_last_game}"; Güncel puan: "${ranking_in_tier}" .`)
-	
-}
 	}
+		active1 = false;
+		setTimeout(() => {
+			active1 = true;
+		}, 10000);
+	} else {
+		console.log(` cooldown` );
+	}
+	
+
+}}
